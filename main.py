b5a2648599748de2bbd32bf5a37430752e9fde8e
Artemiy <artoym13571@gmail.com>
2025-11-08 02:30:59 +0300

    Добавлена проверка обработки исключения

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from json_read_write import read_json, write_json
 from Review import Review
 from datetime import datetime
+from Course import Course
+from Student import Student
+from Exceptions import *
 
 def main():
     filename = "json_file.json"
@@ -18,7 +21,6 @@ def main():
             print(f"Урок: {lesson.title}")
         for review in course.reviews:
             print(f"Отзыв: {review.rating}/5 — {review.comment}")
-
     course = student.enrolled_courses[0]
     new_review = Review(
         id_review=len(course.reviews) + 1,
@@ -30,8 +32,14 @@ def main():
     )
     course.reviews.append(new_review)
     print("\nДобавлен новый отзыв к курсу.")
-
     write_json(filename, students)
+    course = Course(2, "Основы Java", "программирование", "Крутой курс, всем советуем")
+    student = Student(5, "Иван Смирнов", email='Ivan@mail.ru', password='qwerty111')
+    try:
+        student.enroll(course)
+        student.enroll(course)
+    except EnrollmentError as e:
+        print(f"Ошибка: {e}")
 
 
 if __name__ == "__main__":
